7e54977edf6d469852e3a144dae6459b4caab3bc
Melvin-Leroy <melv.leroy@gmail.com>
2020-11-09 16:51:45 +0100

    adding informations to the class and methods.

--- utils/game.py
+++ utils/game.py
@@ -1,6 +1,10 @@
 from utils import player
 
 class Board:
+    """
+    Class that create a game board with the list of players, a turn count, the cards that were played this turn and
+    all the card that were played during the game.
+    """
     def __init__(self):
         self.players = []
         self.turn_count = 0
@@ -8,6 +12,9 @@ class Board:
         self.history_card = []
 
     def start_game(self):
+        """
+        Start the game using all classes and methods defined in the others files.
+        """
         print("The game starts !")
         while True:
             player_name = input("Enter a player name or 'end' if you entered all of them:")
@@ -30,6 +37,7 @@ class Board:
             print("turn nbr : {} \nCards played this turn : {}\nNumber of cards played in the game : {}".format(
                 self.turn_count, self.active_cards, len(self.history_card)
             ))
+        print("You have no cards left, it's the end !")
 
 
 
